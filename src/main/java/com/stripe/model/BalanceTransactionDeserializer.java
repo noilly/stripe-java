faa14b5ff39281d86a22451411b7fa8355c9ab14
Olivier Bellone <ob@stripe.com>
2017-06-09 21:06:29 +0200

    Support all possible balance_transaction source types

--- src/main/java/com/stripe/model/BalanceTransactionDeserializer.java
+++ src/main/java/com/stripe/model/BalanceTransactionDeserializer.java
@@ -1,7 +1,5 @@
 package com.stripe.model;
 
-import java.lang.reflect.Type;
-
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -12,14 +10,31 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonPrimitive;
 
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
 public class BalanceTransactionDeserializer implements JsonDeserializer<BalanceTransaction> {
+
+	@SuppressWarnings("rawtypes")
+	static final Map<String, Class> objectMap = new HashMap<String, Class>();
+	static {
+		objectMap.put("application_fee", ApplicationFee.class);
+		objectMap.put("charge", Charge.class);
+		objectMap.put("dispute", Dispute.class);
+		objectMap.put("fee_refund", FeeRefund.class);
+		objectMap.put("payout", Payout.class);
+		objectMap.put("refund", Refund.class);
+		objectMap.put("transfer", Transfer.class);
+		objectMap.put("transfer_reversal", Reversal.class);
+	}
 
 	@Override
 	public BalanceTransaction deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
 		Gson gson = new GsonBuilder()
 				.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
 				.create();
-		
+
 		if (json.isJsonNull()) {
 			return null;
 		}
@@ -27,15 +42,15 @@ public class BalanceTransactionDeserializer implements JsonDeserializer<BalanceT
 		if (!json.isJsonObject()) {
 			throw new JsonParseException("BalanceTransaction type was not an object, which is problematic.");
 		}
-		
+
 		JsonObject btAsJsonObject = json.getAsJsonObject();
 
 		JsonElement source = btAsJsonObject.get("source");
-		
+
 		btAsJsonObject.remove("source");
 
 		BalanceTransaction balanceTransaction = gson.fromJson(json, typeOfT);
-		
+
 		String sourceId = null;
 
 		if (source.isJsonPrimitive()) {
@@ -50,15 +65,8 @@ public class BalanceTransactionDeserializer implements JsonDeserializer<BalanceT
 			sourceId = sourceIdEl != null ? sourceIdEl.getAsString() : null;
 			JsonElement val = sourceJsonObject.get("object");
 			if (val != null) {
-				Class<? extends HasId> sourceObjClass = null;
-				if ("charge".equals(val.getAsString())) {
-					sourceObjClass = Charge.class;
-				} else if ("transfer".equals(val.getAsString())) {
-					sourceObjClass = Transfer.class;
-				} else if ("refund".equals(val.getAsString())) {
-					sourceObjClass = Refund.class;
-				}
-				// TODO support other source types (?)
+				String type = val.getAsString();
+				Class<? extends HasId> sourceObjClass = objectMap.get(type);
 				if (sourceObjClass != null) {
 					HasId sourceObj = context.deserialize(source, sourceObjClass);
 					balanceTransaction.setSourceObject(sourceObj);
@@ -67,7 +75,7 @@ public class BalanceTransactionDeserializer implements JsonDeserializer<BalanceT
 		} else if (!source.isJsonNull()) {
 			throw new JsonParseException("Source field on a balance transaction was a non-primitive, non-object type.");
 		}
-		
+
 		balanceTransaction.setSource(sourceId);
 
 		return balanceTransaction;
